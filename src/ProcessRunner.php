776145c4462542e995611d05e04bd9e7d35a582e
GeneralVini <generalvini@gmail.com>
2026-09-13 12:57:29 -0300

    fix: alinha rótulos do renderer visual

--- src/ProcessRunner.php
+++ src/ProcessRunner.php
@@ -81,15 +81,15 @@ final class FindingRenderer
             $where = $finding['file'] . ($finding['line'] > 0 ? ':' . $finding['line'] : '');
 
             echo '╭─ ' . $tool . ' ' . str_repeat('─', max(8, 61 - strlen($tool))) . PHP_EOL;
-            echo '│ Arquivo   ' . $where . PHP_EOL;
-            echo '│ Regra     ' . $finding['rule'] . PHP_EOL;
+            echo '│ Arquivo:   ' . $where . PHP_EOL;
+            echo '│ Regra:     ' . $finding['rule'] . PHP_EOL;
             echo '│' . PHP_EOL;
-            echo '│ Problema' . PHP_EOL;
+            echo '│ Corrigir:' . PHP_EOL;
             self::renderWrapped($finding['problem']);
 
             if ($withCorrection) {
                 echo '│' . PHP_EOL;
-                echo '│ Correção' . PHP_EOL;
+                echo '│ Correção:' . PHP_EOL;
                 self::renderWrapped($finding['correction']);
             }
 
