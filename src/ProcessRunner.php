3030b8413290a72594d753624f82fcd4f6269e6c
GeneralVini <generalvini@gmail.com>
2026-09-13 12:25:55 -0300

    fix: evita warning bruto ao iniciar ferramenta ausente

--- src/ProcessRunner.php
+++ src/ProcessRunner.php
@@ -7,15 +7,21 @@ final class ProcessRunner
     /** @param list<string> $command */
     public function run(array $command, string $cwd): int
     {
+        if ($command === []) {
+            throw new InvalidArgumentException('Comando vazio.');
+        }
+
         $descriptorSpec = [
             0 => STDIN,
             1 => STDOUT,
             2 => STDERR,
         ];
 
-        $process = proc_open($command, $descriptorSpec, $pipes, $cwd);
+        $process = @proc_open($command, $descriptorSpec, $pipes, $cwd);
         if (!is_resource($process)) {
-            throw new RuntimeException('Falha ao iniciar processo: ' . implode(' ', $command));
+            throw new RuntimeException(
+                'Falha ao iniciar a ferramenta "' . $command[0] . '". Verifique instalação e permissões.',
+            );
         }
 
         return proc_close($process);
